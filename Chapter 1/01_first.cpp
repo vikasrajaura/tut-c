331b47ccbcf6d1b69bb13cf85c8515ea52f0c0c7
vikas rajaura <rajauravikas@gmail.com>
2023-04-01 23:44:51 +0530

    C initial Commit

---
+++ Chapter 1/01_first.cpp
@@ -0,0 +1,16 @@
+# include<stdio.h>
+#include<conio.h>
+/*
+This is our first c program
+which is awesome!
+*/
+void main(){
+    int a;
+    int b;
+    // Declaring variables to store int value
+    int sum;
+    // 'h' --> a character
+    printf("Hello C, I am Vikas");
+    getch();
+
+}
---
+++ Chapter 1/01_practice_a.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int l=3, b=8;
+    int area = l*b;
+    printf("The area of rectangle is %d", area);
+    return 0;
+}
